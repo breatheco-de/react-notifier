4d2196ae091b110d5ce53e28ce2316624987dc28
Alejandro Sanchez <aalejo@gmail.com>
2018-07-23 18:31:17 +0000

    exclude react and react-dom from the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,20 @@ module.exports = {
         }
     ]
   },
+  externals:{
+    'react': {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   }
